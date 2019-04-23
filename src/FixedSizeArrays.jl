cfc73091369d40ca27f5450d7245ad68abf26a24
SimonDanisch <sdanisch@gmail.com>
2019-04-23 14:48:52 +0200

    remove unused

--- src/FixedSizeArrays.jl
+++ src/FixedSizeArrays.jl
@@ -15,7 +15,6 @@ export FixedMatrix
 export Mat
 export Vec
 export Point
-export @fsa
 export FixedVectorNoTuple
 
 const FixedArray = StaticArray
@@ -23,36 +22,6 @@ const FixedVector = StaticVector
 const FixedMatrix = StaticMatrix
 const Mat = SMatrix
 const FixedVectorNoTuple = FieldVector
-
-function fsa_ast(ex)
-    @assert isa(ex, Expr)
-    if ex.head == :vect # Vector
-        return Expr(:call, SVector{length(ex.args)}, Expr(:tuple, ex.args...))
-    elseif ex.head == :hcat # 1 x n
-        s1 = 1
-        s2 = length(ex.args)
-        return Expr(:call, SMatrix{s1, s2}, Expr(:tuple, ex.args...))
-    elseif ex.head == :vcat
-        if isa(ex.args[1], Expr) && ex.args[1].head == :row # n x m
-            # Validate
-            s1 = length(ex.args)
-            s2s = map(i -> ((isa(ex.args[i], Expr) && ex.args[i].head == :row) ? length(ex.args[i].args) : 1), 1:s1)
-            s2 = minimum(s2s)
-            if maximum(s2s) != s2
-                error("Rows must be of matching lengths")
-            end
-
-            exprs = [ex.args[i].args[j] for i = 1:s1, j = 1:s2]
-            return Expr(:call, SMatrix{s1, s2}, Expr(:tuple, exprs...))
-        else # n x 1
-            return Expr(:call, SMatrix{length(ex.args), 1}, Expr(:tuple, ex.args...))
-        end
-    end
-end
-macro fsa(ex)
-    expr = fsa_ast(ex)
-    esc(expr)
-end
 
 function unit(::Type{T}, i::Integer) where T <: StaticVector
     T(ntuple(Val(length(T))) do j
