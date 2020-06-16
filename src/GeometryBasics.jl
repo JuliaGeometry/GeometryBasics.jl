cc400b72a04ee5294262ea6e970970fe17ee8c88
ARSH SHARMA <184517@nith.ac.in>
2020-06-16 18:30:14 +0530

    Add LineStringMeta, MultiPolygonMeta

--- src/GeometryBasics.jl
+++ src/GeometryBasics.jl
@@ -29,7 +29,7 @@ module GeometryBasics
     export OffsetInteger, ZeroIndex, OneIndex, GLIndex
     export FaceView, SimpleFaceView
     export AbstractPoint, PointMeta, PointWithUV
-    export PolygonMeta, MultiPointMeta, MultiLineStringMeta, MeshMeta
+    export PolygonMeta, MultiPointMeta, MultiLineStringMeta, MeshMeta, LineStringMeta, MultiPolygonMeta
     export decompose, coordinates, faces, normals, decompose_uv, decompose_normals, texturecoordinates
     export Tesselation, pointmeta, Normal, UV, UVW
     export GLTriangleFace, GLNormalMesh3D, GLPlainTriangleMesh, GLUVMesh3D, GLUVNormalMesh3D
