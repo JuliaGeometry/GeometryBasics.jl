80ed4423d966b79b43f3b8f9483c8d05be03c206
SimonDanisch <sdanisch@protonmail.com>
2020-08-05 11:31:08 +0200

    update comment

--- src/metadata.jl
+++ src/metadata.jl
@@ -121,8 +121,7 @@ macro meta_type(name, mainfield, supertype, params...)
                 end
             end
             nt = values(meta)
-            # get the first element to get the per element named tuple type
-            # ElementNT = typeof(map(first, nt))
+            # figure out the NamedTuple type for the struct of arrays element type
             eltypes = map(eltype, values(nt))
             ElementNT = NamedTuple{keys(nt),Tuple{eltypes...}}
             return StructArray{MetaType(XX, ElementNT)}(($(mainfield) = elements, nt...))
