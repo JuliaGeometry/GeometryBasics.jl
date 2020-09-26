e3dbb4768dec4ba02f2bd33e8bc71b977a7ab90c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-09-26 07:29:47 -0300

    Remove Particle

--- src/GeometryBasics.jl
+++ src/GeometryBasics.jl
@@ -13,7 +13,6 @@ include("primitives/rectangles.jl")
 include("primitives/spheres.jl")
 include("primitives/cylinders.jl")
 include("primitives/pyramids.jl")
-include("primitives/particles.jl")
 
 include("interfaces.jl")
 include("metadata.jl")
--- src/primitives/particles.jl
+++
@@ -1,4 +0,0 @@
-struct Particle{N,T} <: GeometryPrimitive{N,T}
-    position::Point{N,T}
-    velocity::Vec{N,T}
-end
