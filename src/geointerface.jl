5133a8dbdfae9a08879c0cd000e7d845c78ffc06
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-01-26 16:43:48 +0530

    Optimize centroid finding by using known signed area of linestring

--- src/geointerface.jl
+++ src/geointerface.jl
@@ -296,17 +296,33 @@ function centroid(ls::LineString{2, T}) where T
     return centroid ./ total_area
 end
 
+# a more optimized function, so we only calculate signed area once!
+function centroid_and_signed_area(ls::LineString{2, T}) where T
+    centroid = Point{2, T}(0)
+    total_area = T(0)
+    if length(ls) == 1
+        return sum(ls[1])/2
+    end
+
+    p0 = ls[1][1]
+
+    for i in 1:(length(ls)-1)
+        p1 = ls[i][2]
+        p2 = ls[i+1][2]
+        area = signed_area(p0, p1, p2)
+        centroid = centroid .+ Point{2, T}((p0[1] + p1[1] + p2[1])/3, (p0[2] + p1[2] + p2[2])/3) * area
+        total_area += area
+    end
+    return (centroid ./ total_area, total_area)
+end
+
 function centroid(poly::GeometryBasics.Polygon{2, T}) where T
-    exterior_points = decompose(Point2f, poly.exterior)
-    exterior_centroid = centroid(poly.exterior)
-    exterior_area = signed_area(poly.exterior)
+    exterior_centroid, exterior_area = centroid_and_signed_area(poly.exterior)
 
     total_area = exterior_area
     interior_numerator = Point{2, T}(0)
     for interior in poly.interiors
-        interior_points = decompose(Point2f, interior)
-        interior_centroid = centroid(interior)
-        interior_area = signed_area(interior_points)
+        interior_centroid, interior_area = centroid_and_signed_area(interior)
         total_area += interior_area
         interior_numerator += interior_centroid * interior_area
     end
@@ -316,14 +332,11 @@ function centroid(poly::GeometryBasics.Polygon{2, T}) where T
 end
 
 function centroid(multipoly::MultiPolygon)
-
     centroids = centroid.(multipoly.polygons)
-
     areas = signed_area.(multipoly.polygons)
     areas ./= sum(areas)
 
     return sum(centroids .* areas) / sum(areas)
-
 end
 
 
