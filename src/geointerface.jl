6b738a94c2bdfe7adf9e1d034860f61c932e5a68
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-01-26 22:24:42 +0530

    Fix contains(polygon, point) - using Luxor's implementation

--- src/geointerface.jl
+++ src/geointerface.jl
@@ -360,7 +360,7 @@ function GeoInterface.contains(
     point::Point{2, T2}
     ) where {T1, T2} 
 
-    contains(ls, point)
+    contains(geom, point)
 
 end
 
@@ -368,55 +368,49 @@ end
 #            Implementation            #
 ########################################
 
-# test if point B is on the line defined by A and C
-function is_on_line(A::Point2, B::Point2, C::Point2)
-   # if AC is vertical
-   if (A[1] == C[1]) 
-        return B[1] == C[1]
-   # if AC is horizontal
-   elseif (A[2] == C[2]) 
-        return B[2] == C[2]
-   end
-   # match the gradients
-   return (A[1] - C[1])*(A[2] - C[2]) == (C[1] - B[1])*(C[2] - B[2])
-end
+_cross(p1, p2, p3) = (p1[1] - p3[1]) * (p2[2] - p3[2]) - (p2[1] - p3[1]) * (p1[2] - p3[2])
 
+# Implementation of a point-in-polygon algorithm
+# from Luxor.jl.  This is the Hormann-Agathos (2001) algorithm.
+# For the source, see https://github.com/JuliaGraphics/Luxor.jl/blob/66d60fb51f6b1bb38690fe8dcc6c0084eeb80710/src/polygons.jl#L190-L229.
 function contains(ls::GeometryBasics.LineString{2, T1}, point::Point{2, T2}) where {T1, T2}
-
-    # the original C code from https://wrfranklin.org/Research/Short_Notes/pnpoly.html
-    # int pnpoly(int npol, float *xp, float *yp, float x, float y)
-    # {
-    #   i = 1
-    #   j = 1
-    #   c = false
-    #   for (i = 0, j = npol-1; i < npol; j = i++) {
-    #     if ((((yp[i]<=y) && (y<yp[j])) ||
-    #          ((yp[j]<=y) && (y<yp[i]))) &&
-    #         (x < (xp[j] - xp[i]) * (y - yp[i]) / (yp[j] - yp[i]) + xp[i]))
-
-    #       c = !c;
-    #   }
-    #   return c;
-
-    # }
-    x, y = point
-
+    pointlist = decompose(Point{2, promote_type(T1, T2)}, ls)
     c = false
-    @inbounds for (p1, p2) in ls
-        # handle vertex and edge cases
-        if p1 == point || p2 == point
-            return true
-        elseif (p1[1] < x < p2[1] && p1[2] < y < p2[2]) || (p2[1] < x < p1[1] && p2[2] < y < p1[2]) # point is in bbox of line
-            return is_on_line(p1, point, p2)
+    @inbounds for counter in eachindex(pointlist)
+        q1 = pointlist[counter]
+        # if reached last point, set "next point" to first point
+        if counter == length(pointlist)
+            q2 = pointlist[1]
+        else
+            q2 = pointlist[counter + 1]
         end
-        # Hormann-Agathos ray casting method
-        if ((p1[2] ≤ y && y < p2[2]) ||
-            (p2[2] ≤ y && y < p1[2])) &&
-            (x < (p2[1] - p1[1]) * (y - p1[2]) / (p2[2] - p1[2]) + p1[1])
-            c = !c
+        if q1 == point
+            # allowonedge || error("isinside(): VertexException a")
+            continue
+        end
+        if q2[2] == point[2]
+            if q2[1] == point[1]
+                # allowonedge || error("isinside(): VertexException b")
+                continue
+            elseif (q1[2] == point[2]) && ((q2[1] > point[1]) == (q1[1] < point[1]))
+                # allowonedge || error("isinside(): EdgeException")
+                continue
+            end
+        end
+        if (q1[2] < point[2]) != (q2[2] < point[2]) # crossing
+            if q1[1] >= point[1]
+                if q2[1] > point[1]
+                    c = !c
+                elseif ((_cross(q1, q2, point) > 0) == (q2[2] > q1[2]))
+                    c = !c
+                end
+            elseif q2[1] > point[1]
+                if ((_cross(q1, q2, point) > 0) == (q2[2] > q1[2]))
+                    c = !c
+                end
+            end
         end
     end
-
     return c
 
 end
@@ -425,14 +419,13 @@ function contains(poly::Polygon{2, T1}, point::Point{2, T2}) where {T1, T2}
     c = contains(poly.exterior, point)
     for interior in poly.interiors
         if contains(interior, point)
-            c = false # if hole contains point, then point is not contained in poly.
-            break
+            return false
         end
     end
     return c
 end
 
-# TODOs: implement contains for mesh, 
+# TODOs: implement contains for mesh, simplex, and 3d objects (eg rect, triangle, etc.)
 
 contains(mp::MultiPolygon{2, T1}, point::Point{2, T2}) where {T1, T2} = any((contains(poly, point) for poly in mp.polygons))
 
