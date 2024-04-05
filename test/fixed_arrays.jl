9f3378a2e7203737075619e797122e74ac6c9ec4
Anshul Singhvi <asinghvi17@simons-rock.edu>
2024-04-05 16:20:00 -0400

    Add tests

--- test/fixed_arrays.jl
+++ test/fixed_arrays.jl
@@ -14,3 +14,20 @@ end
         @test [T(0, 0), T(5, 0)] == x .- T(2, 3)
     end
 end
+
+@testset "finite, nan, inf tests" begin
+    for T in (Vec, Point)
+        @testset "$T" begin
+            nan_point = T(Float64.((1.0, 2.0, 3.0, NaN)))
+            inf_point = T(Float64.((1.0, 2.0, Inf, 4.0)))
+            @test isinf(inf_point)
+            @test !isinf(nan_point)
+            @test isnan(nan_point)
+            @test !isnan(inf_point)
+            @test !isfinite(nan_point)
+            @test !isfinite(inf_point)
+            @test !isfinite(nan_point + inf_point)
+            @test isfinite(T(1, 2, 3))
+        end
+    end
+end
