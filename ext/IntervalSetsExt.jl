305c8566118bcb9802632dcaef5e20a1e0e9566b
Alexander Plavin <alexander@plav.in>
2025-08-23 22:31:25 -0400

    closed intervals only

--- ext/IntervalSetsExt.jl
+++ ext/IntervalSetsExt.jl
@@ -3,12 +3,12 @@ module IntervalSetsExt
 using IntervalSets
 using GeometryBasics
 
-GeometryBasics.HyperRectangle(ints::Vararg{Interval, N}) where {N} = HyperRectangle{N}(ints...)
-GeometryBasics.HyperRectangle{N}(ints::Vararg{Interval, N}) where {N} = HyperRectangle{N}(
+GeometryBasics.HyperRectangle(ints::Vararg{ClosedInterval, N}) where {N} = HyperRectangle{N}(ints...)
+GeometryBasics.HyperRectangle{N}(ints::Vararg{ClosedInterval, N}) where {N} = HyperRectangle{N}(
     Vec(leftendpoint.(ints)),
     Vec(rightendpoint.(ints) .- leftendpoint.(ints))
 )
-GeometryBasics.HyperRectangle{N,T}(ints::Vararg{Interval, N}) where {N,T} = HyperRectangle{N,T}(
+GeometryBasics.HyperRectangle{N,T}(ints::Vararg{ClosedInterval, N}) where {N,T} = HyperRectangle{N,T}(
     Vec(leftendpoint.(ints)),
     Vec(rightendpoint.(ints) .- leftendpoint.(ints))
 )
