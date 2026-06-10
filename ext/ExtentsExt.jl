6b45ee6cb21c3bf8f03d597c4419bf8a00ed9a90
ffreyer <frederic481994@hotmail.de>
2026-06-10 16:45:09 +0200

    fix error

--- ext/ExtentsExt.jl
+++ ext/ExtentsExt.jl
@@ -13,7 +13,7 @@ function Extents.extent(rect::GeometryBasics.Rect3)
     return Extents.Extent(X=(xmin, xmax), Y=(ymin, ymax), Z=(zmin, zmax))
 end
 
-function Extents.extent(rect::Rect4)
+function Extents.extent(rect::GeometryBasics.Rect4)
     (xmin, ymin, zmin, mmin), (xmax, ymax, zmax, mmax) = extrema(rect)
     return Extents.Extent(X=(xmin, xmax), Y=(ymin, ymax), Z=(zmin, zmax), M=(mmin, mmax))
 end
